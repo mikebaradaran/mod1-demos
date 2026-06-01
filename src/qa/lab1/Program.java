255b729d644a3664d10678ccf7777dd3f9e3ff7f
mike Baradaran <mike.baradaran@qa.com>
2026-06-01 10:40:39 +0100

    changed code...

--- src/qa/lab1/Program.java
+++ src/qa/lab1/Program.java
@@ -1,10 +1,21 @@
 package qa.lab1;
 
+import java.io.FileReader;
+
+import qa.training.Student;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		System.out.println("Hello World again!");
+		
+		// qa.lab1.Student stu1;
+		
+//		Student stu2;
+//		
+//		FileReader x;
+		
+		
 	}
 
 }
